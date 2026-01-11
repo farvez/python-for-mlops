13325bc104f803c57dc7d8b784226ef328d1b1e9
Admin <farvezanzam03@gmail.com>
2026-01-11 23:51:13 +0530

    Day 1: Python Basics

--- Day_1_Python_Basics/control_flow.py
+++ Day_1_Python_Basics/control_flow.py
@@ -3,7 +3,7 @@ accuracy = 0.82
 if accuracy > 0.8:
     print("Model is acceptable")
 else:
-    print("Model needs improvement")
+    print("Model needs an improvement")
 
 # Real use:
 # Stop pipeline if accuracy < threshold
