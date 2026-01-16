a3ac6d876e8ad9cf821fd6e89d594f5283f7db75
Admin <farvezanzam03@gmail.com>
2026-01-16 21:08:08 +0530

    Day 8

---
+++ Day_8_real_ml_pipeline/data_loader.py
@@ -0,0 +1,17 @@
+import csv
+
+def load_data(path):
+    X, y = [], []
+
+    with open(path) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            X.append([int(row["age"]), [int(row["salary"])]])
+            y.append([int(row["label"])])
+    return X, y
+
+# if __name__ == "__main__":
+#     path = "C:\\Users\\Admin\\Downloads\\Python for MLOps\\Day_8_real_ml_pipeline\\data\\dataset.csv"
+
+#     cat = load_data(path)
+#     print(cat)
